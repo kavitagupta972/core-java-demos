ad778a0bb00ced125d266fb8cd1f08b954c87246
kavitagupta972 <kavitagupta972@gmail.com>
2021-07-19 16:27:45 +0530

    Adding java files

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+	public static void main(String[] arg) {
+		String greetings = "Hello JAVA World!!!!";
+		printMessage(greetings);
+		
+	}
+	public static void printMessage(String message){
+		int sum = 20;
+		float sum1 = 20.87f;
+		char ch = 'K';
+		boolean flag = true;
+		System.out.println(message +" "+  sum +" "+ sum1+" "+ch+" "+flag);
+	}
+}
