c548f6bf36dbd47dffd41eeff0aa6dfc2a6de963
kavitagupta972 <kavitagupta972@gmail.com>
2021-07-20 16:26:05 +0530

    Adding super constructor demo

--- inheritance/BasicDemo.java
+++ inheritance/BasicDemo.java
@@ -1,21 +1,31 @@
-class Vehicle {
-	String color = "Green";
+ class Vehicle {
+	String color;
+	public Vehicle(String color) {
+		this.color = color;
+	}
 	public void moving() {
 		System.out.println("Vehicle is moving");
 	}
 }
 
 class TwoWheeler extends Vehicle {
-	int no_of_tyres = 2;
+
+	int no_of_tyres;
+
+	public TwoWheeler(int n, String color){
+		super(color);
+		no_of_tyres = n;
+	}
 	public void printDetail() {
 		System.out.println("Color of vehicle is "+ color);
+		System.out.println("Tyres in vehicle is "+ no_of_tyres);
 		moving();
 	}
 }
 
 class BasicDemo {
 	public static void main(String[] arg) {
-			TwoWheeler obj = new TwoWheeler();
+			TwoWheeler obj = new TwoWheeler(2, "Yellow");
 			obj.printDetail();
 	}
 }
