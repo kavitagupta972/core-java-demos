f844eb8b002fe1f9f535a05fbabbc5cd3b5bc296
kavitagupta972 <kavitagupta972@gmail.com>
2021-07-20 19:56:09 +0530

    Adding multiple catch demo

--- Exception Handling/FinallyDemo.java
+++ Exception Handling/FinallyDemo.java
@@ -3,18 +3,22 @@ class MultipleCatchDemo
   	public static void main(String[] args)
   	{
      	int x=10, y = 2;
+       FileReader fr=null;
      	try{
      		int result = x/y;
         int arr[] = new int[5];
         System.out.println(arr[7]);
+        //resources
+         fr  = new FileReader("file name");
 
      	}catch(ArithmeticException exception) {
      		System.out.println("Exception has occured while dividing x/y " + exception);
      	} catch(ArrayIndexOutOfBoundsException e){
         System.out.println("Error in arr array of out of index ");
-      }catch(Exception e) {
-     		System.out.println("Generic exception has occured " + e);
-     	}
-
+      }finally {
+        //close resources
+        fr.close();
+        System.out.println("Closing the opened connection before exiting");
+      }
   	}
 }
